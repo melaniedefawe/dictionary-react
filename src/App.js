076aed0c3121186e69b9856b5874742b51af2c52
valhalyria <melanie.defawe@gmail.com>
2022-07-15 13:42:51 +0200

    Created header + footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import header from "./header.png";
+import footer from "./footer.png";
+import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
+      <div className="container">
+        <header>
+          <img src={header} alt="header image" />
+        </header>
+        <h1>Dictionary</h1>
+        <Search />
+        <footer>
+          <p>
+            <a
+              href="https://github.com/valhalyria/dictionary-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source project
+            </a>{" "}
+            coded by Melanie D.
+          </p>
+          <img src={footer} alt="footer image" />
+        </footer>
+      </div>
     </div>
   );
 }
