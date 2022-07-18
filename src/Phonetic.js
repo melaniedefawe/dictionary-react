14e6ffad865c77aa2dceca18a6a17415eccc0ae4
valhalyria <melanie.defawe@gmail.com>
2022-07-18 15:53:40 +0200

    Hid audio player when no audio file

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
-  return (
-    <div>
-      <span className="phonetic">{props.phonetic.text}</span>
+  if (props.phonetic.audio) {
+    return (
+      <div>
+        <span className="phonetic">{props.phonetic.text}</span>
+        <span>
+          <ReactAudioPlayer src={props.phonetic.audio} controls />
+        </span>
+      </div>
+    );
+  } else {
+    return (
       <span>
-        <ReactAudioPlayer src={props.phonetic.audio} controls />
+        <span className="phonetic">{props.phonetic.text}</span>
       </span>
-    </div>
-  );
+    );
+  }
 }
